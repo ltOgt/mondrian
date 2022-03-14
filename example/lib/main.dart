54e2e98d02ea7085c260c229071ce02486f96239
ltOgt <omnesiaorg@gmail.com>
2022-03-14 10:35:04 +0100

    boilerplate

---
+++ example/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:ltogt_utils_flutter/ltogt_utils_flutter.dart';
+import 'package:mondrian/mondrian.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mondrian Example',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: MondrianWM(
+          tree: tree,
+          initialAxis: Axis.vertical,
+          onResize: (path, newFraction) {},
+          resolveLeafToWidget: (id) => WindowExample(id.value),
+        ),
+      ),
+    );
+  }
+}
+
+const tree = WindowManagerTree(
+  rootNode: WindowManagerBranch(
+    fraction: 1,
+    children: [
+      WindowManagerBranch(
+        fraction: .7,
+        children: [
+          WindowManagerLeaf(
+            fraction: .7,
+            id: WindowManagerLeafId("Big top left"),
+          ),
+          WindowManagerBranch(
+            fraction: .3,
+            children: [
+              WindowManagerLeaf(
+                fraction: .6,
+                id: WindowManagerLeafId("Medium Top Right"),
+              ),
+              WindowManagerLeaf(
+                fraction: .6,
+                id: WindowManagerLeafId("Small Mid Right"),
+              ),
+            ],
+          ),
+        ],
+      ),
+      WindowManagerBranch(
+        fraction: .3,
+        children: [
+          WindowManagerLeaf(
+            fraction: .3,
+            id: WindowManagerLeafId("Bottom Left"),
+          ),
+          WindowManagerLeaf(
+            fraction: .3,
+            id: WindowManagerLeafId("Bottom Mid"),
+          ),
+          WindowManagerLeaf(
+            fraction: .4,
+            id: WindowManagerLeafId("Bottom Right"),
+          ),
+        ],
+      )
+    ],
+  ),
+);
+
+class WindowExample extends StatelessWidget {
+  const WindowExample(this.text);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // decoration: BoxDecoration(
+      //   border: Border.all(color: Colors.blueAccent),
+      // ),
+      child: Center(
+        child: AutoSizeText(text: text),
+      ),
+    );
+  }
+}
+
+/*
+
+
+________________________________
+                        |
+                        |
+                        |_______
+                        |
+________________________|_______
+          |         |
+__________|_________|___________
+
+
+Column(
+  0.70: Row(
+    0.70: WINDOW
+    0.30: Column(
+      0.60: WINDOW
+      0.40: WINDOW
+    )
+  )
+  0.30: Row(
+    0.30: WINDOW
+    0.30: WINDOW
+    0.40: WINDOW
+  )
+)
+*/
