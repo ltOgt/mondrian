2d5e75d12693a5f9aa7172233496241ce068ed66
ltOgt <omnesiaorg@gmail.com>
2022-03-19 20:27:59 +0100

    add mondrian example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,10 +15,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var tree = k_tree;
+  var baseExampleTree = base_tree;
+  bool isBaseExample = false;
 
-  MondrianTreeLeafId? movingId;
-  List<int>? lastMovingPath;
+  var mondrianExampleTree = mondrian_tree;
+  final mondrianExampleColors = <MondrianTreeLeafId, Color>{
+    const MondrianTreeLeafId("Red 1"): Colors.red,
+    const MondrianTreeLeafId("Blue 1"): Colors.blue,
+    const MondrianTreeLeafId("White 1"): Colors.white,
+    const MondrianTreeLeafId("Yellow 1"): Colors.yellow,
+    const MondrianTreeLeafId("White 2"): Colors.white,
+    const MondrianTreeLeafId("White 3"): Colors.white,
+    const MondrianTreeLeafId("Yellow 2"): Colors.yellow,
+    const MondrianTreeLeafId("Blue 2"): Colors.blue,
+    const MondrianTreeLeafId("Yellow 3"): Colors.yellow,
+    const MondrianTreeLeafId("White 4"): Colors.white,
+    const MondrianTreeLeafId("Red 2"): Colors.red,
+    const MondrianTreeLeafId("White 5"): Colors.white,
+  };
 
   void toggleDebugPaint() {
     MondrianDebugSingleton.instance.toggleBranchDebugPainting();
@@ -31,29 +45,71 @@ class _MyAppState extends State<MyApp> {
       title: 'Mondrian Example',
       theme: ThemeData.dark(),
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: toggleDebugPaint,
-          child: const Icon(Icons.brush),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: toggleDebugPaint,
+              child: const Icon(Icons.brush),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  isBaseExample = !isBaseExample;
+                });
+              },
+              child: const Icon(Icons.grid_3x3),
+            ),
+          ],
         ),
         body: MondrianWidget(
-          tree: tree,
+          tree: isBaseExample ? baseExampleTree : mondrianExampleTree,
+          resizeDraggerWidth: isBaseExample ? 2 : 10.0,
+          resizeDraggerColor: isBaseExample ? const Color(0xFFAAAAFF) : Colors.black,
           onUpdateTree: (tree) {
             setState(() {
-              this.tree = tree;
+              if (isBaseExample) {
+                baseExampleTree = tree;
+              } else {
+                mondrianExampleTree = tree;
+              }
             });
           },
+          buildLeafBar: isBaseExample
+              ? null
+              : (leafPath) {
+                  final _leaf = (mondrianExampleTree.extractPath(leafPath) as MondrianTreeLeaf);
+                  return Container(
+                      color: Colors.black.withAlpha(5),
+                      child: Center(
+                        child: AutoSizeText(
+                          text: _leaf.id.value,
+                        ),
+                      ));
+                },
+          targetPositionIndicator: isBaseExample //
+              ? const DecoratedBox(decoration: BoxDecoration(color: Color(0xAAFFFFFF)))
+              : const DecoratedBox(decoration: BoxDecoration(color: Color(0xAA000000))),
           buildLeaf: (path, tabIndex) {
+            final tree = isBaseExample ? baseExampleTree : mondrianExampleTree;
             final _leaf = (tree.extractPath(path) as MondrianTreeLeaf);
 
             final leafId = tabIndex == null //
                 ? _leaf.id
                 : (_leaf as MondrianTreeTabLeaf).tabs[tabIndex];
 
-            return Center(
-              child: AutoSizeText(
-                text: leafId.value,
-              ),
-            );
+            if (isBaseExample) {
+              return Center(
+                child: AutoSizeText(
+                  text: leafId.value,
+                ),
+              );
+            } else {
+              return Container(
+                color: mondrianExampleColors[leafId],
+              );
+            }
           },
         ),
       ),
@@ -61,7 +117,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-final k_tree = MondrianTree(
+final base_tree = MondrianTree(
   rootAxis: MondrianAxis.vertical,
   rootNode: MondrianTreeBranch(
     fraction: 1,
@@ -110,6 +166,95 @@ final k_tree = MondrianTree(
           ),
         ],
       )
+    ],
+  ),
+);
+
+const mondrian_tree = MondrianTree(
+  rootAxis: MondrianAxis.horizontal,
+  rootNode: MondrianTreeBranch(
+    fraction: 1,
+    children: [
+      // red blue
+      MondrianTreeBranch(
+        fraction: 1 / 6,
+        children: [
+          MondrianTreeLeaf(fraction: 2 / 3, id: MondrianTreeLeafId("Red 1")),
+          MondrianTreeLeaf(fraction: 1 / 3, id: MondrianTreeLeafId("Blue 1")),
+        ],
+      ),
+      // rest
+      MondrianTreeBranch(
+        fraction: 5 / 6,
+        children: [
+          // White Yellow
+          MondrianTreeBranch(
+            fraction: 1 / 4,
+            children: [
+              MondrianTreeLeaf(fraction: 2 / 3, id: MondrianTreeLeafId("White 1")),
+              MondrianTreeLeaf(fraction: 1 / 3, id: MondrianTreeLeafId("Yellow 1")),
+            ],
+          ),
+          // Rest
+          MondrianTreeBranch(
+            fraction: 3 / 4,
+            children: [
+              // YELLOW BLUE PPPARENT
+              MondrianTreeBranch(
+                fraction: 1 / 2,
+                children: [
+                  // YELLOW BLUE PPARENT
+                  MondrianTreeBranch(
+                    fraction: 4 / 7,
+                    children: [
+                      // WHITE
+                      MondrianTreeLeaf(fraction: 2 / 5, id: MondrianTreeLeafId("White 3")),
+                      // YELLOW BLUE PARENT
+                      MondrianTreeBranch(
+                        fraction: 3 / 5,
+                        children: [
+                          // YELLOW
+                          MondrianTreeLeaf(fraction: 2 / 3, id: MondrianTreeLeafId("Yellow 2")),
+                          // BLUE
+                          MondrianTreeLeaf(fraction: 1 / 3, id: MondrianTreeLeafId("Blue 2")),
+                        ],
+                      ),
+                    ],
+                  ),
+                  // WHITE
+                  MondrianTreeLeaf(fraction: 3 / 7, id: MondrianTreeLeafId("White 2")),
+                ],
+              ),
+              // RED YELLOW ANCESTOR
+              MondrianTreeBranch(
+                fraction: 1 / 2,
+                children: [
+                  // RED WITH REST
+                  MondrianTreeBranch(
+                    fraction: 4 / 5,
+                    children: [
+                      // RED WHITE
+                      MondrianTreeBranch(
+                        fraction: 3 / 4,
+                        children: [
+                          // RED
+                          MondrianTreeLeaf(fraction: 3 / 7, id: MondrianTreeLeafId("Red 2")),
+                          // WHITE
+                          MondrianTreeLeaf(fraction: 4 / 7, id: MondrianTreeLeafId("White 5")),
+                        ],
+                      ),
+                      // WHITE
+                      MondrianTreeLeaf(fraction: 1 / 4, id: MondrianTreeLeafId("White 4")),
+                    ],
+                  ),
+                  // YELLOW
+                  MondrianTreeLeaf(fraction: 1 / 5, id: MondrianTreeLeafId("Yellow 3")),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     ],
   ),
 );
