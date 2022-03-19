2613ea1571a03cf97fd98d99d54e021d1209eb6f
ltOgt <omnesiaorg@gmail.com>
2022-03-19 12:03:59 +0100

    started adding tests, TODO need encode first
    this is because the generated tab id obviously differs
    => need to compare encode()d representations which dont include tab ids

--- test/mondrian_test.dart
+++ test/mondrian_test.dart
@@ -3,8 +3,80 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mondrian/mondrian.dart';
 
 void main() {
+  group("Move Leaf", () {
+    test("Axis switch on root move on other axis", () {
+      const initialTree = MondrianTree(
+        rootAxis: MondrianAxis.vertical,
+        rootNode: MondrianTreeBranch(
+          fraction: 1,
+          children: [
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 1")),
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 2")),
+          ],
+        ),
+      );
+
+      const expectedTreeAfterUpdate = MondrianTree(
+        rootAxis: MondrianAxis.horizontal,
+        rootNode: MondrianTreeBranch(
+          fraction: 1,
+          children: [
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 2")),
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 1")),
+          ],
+        ),
+      );
+
+      final actualTreeAfterUpdate = initialTree.moveLeaf(
+        sourcePath: [1],
+        targetPath: [0],
+        tabIndexIfAny: null,
+        targetSide: MondrianLeafMoveTargetDropPosition.left,
+      );
+
+      expect(expectedTreeAfterUpdate, equals(actualTreeAfterUpdate));
+    });
+
+    test("root change on merge to tab group", () {
+      const initialTree = MondrianTree(
+        rootAxis: MondrianAxis.vertical,
+        rootNode: MondrianTreeBranch(
+          fraction: 1,
+          children: [
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 1")),
+            MondrianTreeLeaf(fraction: .5, id: MondrianTreeLeafId("Leaf 2")),
+          ],
+        ),
+      );
+
+      // TabLeaf can not be const because of internal generated id
+      final expectedTreeAfterUpdate = MondrianTree(
+        rootAxis: MondrianAxis.vertical,
+        rootNode: MondrianTreeTabLeaf(
+          fraction: 1,
+          activeTabIndex: 1,
+          tabs: const [
+            MondrianTreeLeafId("Leaf 1"),
+            MondrianTreeLeafId("Leaf 2"),
+          ],
+        ),
+      );
+
+      final actualTreeAfterUpdate = initialTree.moveLeaf(
+        sourcePath: [1],
+        targetPath: [0],
+        tabIndexIfAny: null,
+        targetSide: MondrianLeafMoveTargetDropPosition.center,
+      );
+
+      // TODO this currently fails because the generated ids of the tab leafs obviously differ
+      //  need to add encode + decode and compare encoded representations (which wont include the generated ids)
+      expect(expectedTreeAfterUpdate, equals(actualTreeAfterUpdate));
+    });
+  });
   test('Update Tree', () {
     const initialTree = MondrianTree(
+      rootAxis: MondrianAxis.vertical,
       rootNode: MondrianTreeBranch(
         fraction: 1,
         children: [
@@ -34,6 +106,7 @@ void main() {
     );
 
     const expectedTreeAfterUpdate = MondrianTree(
+      rootAxis: MondrianAxis.vertical,
       rootNode: MondrianTreeBranch(
         fraction: 1,
         children: [
