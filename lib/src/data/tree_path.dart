9b936961870660f4071b27b358a93ab0c3bf1990
ltOgt <omnesiaorg@gmail.com>
2022-03-31 14:46:16 +0200

    add equality

--- lib/src/data/tree_path.dart
+++ lib/src/data/tree_path.dart
@@ -1,3 +1,6 @@
+import 'package:collection/collection.dart';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 /// The index for each child that must be passed to reach the destination node.
 typedef MondrianTreePath = List<int>;
 
@@ -11,4 +14,22 @@ class MondrianTreePathWithTabIndexIfAny {
     required this.path,
     required this.tabIndexIfAny,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    final listEquals = const DeepCollectionEquality().equals;
+
+    return other is MondrianTreePathWithTabIndexIfAny &&
+        listEquals(other.path, path) &&
+        other.tabIndexIfAny == tabIndexIfAny;
+  }
+
+  @override
+  int get hashCode {
+    final listHash = const DeepCollectionEquality().hash;
+
+    return listHash(path) ^ tabIndexIfAny.hashCode;
+  }
 }
